6f1ec1647a5f9c31a9f23dce5abba3f63a85da42
Dave Duran <dave@yodata.io>
2017-09-02 10:49:52 -0700

    firebase-auth first working version

---
+++ res/home/config.js
@@ -0,0 +1,10 @@
+// todo: need to get the missing values from kubernetes secrets
+firebase.initializeApp({
+    "apiKey": "",
+    "databaseURL": "",
+    "storageBucket": "wiresafe-project.appspot.com",
+    "authDomain": "wiresafe-project.firebaseapp.com",
+    "messagingSenderId": "1039382416339",
+    "projectId": ""
+});
+// console.log('firebase app initialized')
